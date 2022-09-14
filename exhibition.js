0d6b3f268e3cfc34d14bafc799bd5d16b0a68e01
snakiso <igorchagov@mail.ru>
2022-09-14 13:09:41 +0300

    add load

--- exhibition.js
+++ exhibition.js
@@ -1,33 +1,49 @@
 let accordionItem = document.querySelectorAll('.accordion__item')
-let accordionBtn = document.querySelectorAll('.accordion__button') 
+let accordionBtn = document.querySelectorAll('.accordion__button')
 let accordion = document.querySelector('.accordion')
-function addHidden(){
- for (let i = 0; i < accordionItem.length; i++){
-  accordionItem[i].classList.add('hidden')
- }
+function addHidden() {
+    for (let i = 0; i < accordionItem.length; i++) {
+        accordionItem[i].classList.add('hidden')
+    }
 }
 
-for(let i = 0; i<accordionItem.length; i++){
- accordionItem[i].addEventListener('click', () => {
-  addHidden()
-  accordionItem[i].classList.add('active')
-  accordionItem[i].classList.remove('hidden')
-  ChangeActiveSlide() 
- })
+for (let i = 0; i < accordionItem.length; i++) {
+    accordionItem[i].addEventListener('click', () => {
+        addHidden()
+        accordionItem[i].classList.add('active')
+        accordionItem[i].classList.remove('hidden')
+        ChangeActiveSlide()
+    })
 }
-function ChangeActiveSlide(){
-if (accordionItem[0].classList.contains('active') && accordionItem[0].classList.contains('hidden') == 0){
- accordion.style.backgroundImage = "url(./img/russian.png)"
-}
- if (accordionItem[1].classList.contains('active') && accordionItem[1].classList.contains('hidden') == 0){
-  accordion.style.backgroundImage = "url('./img/international.png')"
- }
- if (accordionItem[2].classList.contains('active') && accordionItem[2].classList.contains('hidden') == 0) {
-  accordion.style.backgroundImage = "url('./img/infrastructura.png')"
- }
- if (accordionItem[3].classList.contains('active') && accordionItem[3].classList.contains('hidden') == 0) {
-  accordion.style.backgroundImage = "url('./img/strategy.png')"
- }
+function ChangeActiveSlide() {
+    if (accordionItem[0].classList.contains('active') && accordionItem[0].classList.contains('hidden') == 0) {
+        const img = new Image();
+        img.src = './img/russian.png'
+        img.onload = () => {
+            accordion.style.backgroundImage = `url(${img.src})`
+        }
+    }
+    if (accordionItem[1].classList.contains('active') && accordionItem[1].classList.contains('hidden') == 0) {
+        const img = new Image();
+        img.src = './img/international.png'
+        img.onload = () => {
+            accordion.style.backgroundImage = `url(${img.src})`
+        }
+    }
+    if (accordionItem[2].classList.contains('active') && accordionItem[2].classList.contains('hidden') == 0) {
+        const img = new Image();
+        img.src = './img/infrastructura.png'
+        img.onload = () => {
+            accordion.style.backgroundImage = `url(${img.src})`
+        }
+    }
+    if (accordionItem[3].classList.contains('active') && accordionItem[3].classList.contains('hidden') == 0) {
+        const img = new Image();
+        img.src = './img/strategy.png'
+        img.onload = () => {
+            accordion.style.backgroundImage = `url(${img.src})`
+        }
+    }
 }
 // Код для видео 
 let video = document.querySelector('.back-video')
@@ -39,19 +55,19 @@ let dateShow = document.querySelector('.show-date')
 
 
 window.addEventListener('click', () => {
- video.classList.add('faded')
- video.style.opacity = '0.2'
- header.classList.remove('faded')
- title.classList.remove('faded')
- motto.classList.remove('faded')
+    video.classList.add('faded')
+    video.style.opacity = '0.2'
+    header.classList.remove('faded')
+    title.classList.remove('faded')
+    motto.classList.remove('faded')
 })
 window.addEventListener('scroll', () => {
- video.classList.add('faded')
- video.style.opacity = '0.2'
- header.classList.remove('faded')
- title.classList.remove('faded')
- motto.classList.remove('faded')
- dateShow.classList.remove('faded')
+    video.classList.add('faded')
+    video.style.opacity = '0.2'
+    header.classList.remove('faded')
+    title.classList.remove('faded')
+    motto.classList.remove('faded')
+    dateShow.classList.remove('faded')
 })
 
 
@@ -59,11 +75,11 @@ window.addEventListener('scroll', () => {
 let closeBtn = document.querySelector('.popNav__close-btn')
 let nav = document.querySelector('.popNav')
 let burger = document.querySelector('.header__burger')
-burger.addEventListener('click', () => { 
+burger.addEventListener('click', () => {
     burger.classList.add('active')
     nav.classList.add('active')
 })
-closeBtn.addEventListener('click', () => { 
+closeBtn.addEventListener('click', () => {
     burger.classList.remove('active')
     nav.classList.remove('active')
 })
